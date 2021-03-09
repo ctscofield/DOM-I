9b9089dbdc578f5095de4c1b2b79f2e0d0b0230c
ctscofield <ctscofield@gmail.com>
2021-03-08 19:02:56 -0700

    MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,60 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let circlePic = document.getElementById("cta-img");
+circlePic.setAttribute("src", siteContent["cta"]["img-src"]);
+
+let midPic = document.getElementById("middle-img");
+midPic.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let nav = document.querySelectorAll("nav a");
+
+for(i = 0;i<6;i++) {
+  let navItem = "nav-item-"+(i+1);
+  nav[i].textContent = siteContent["nav"][navItem];
+}
+
+let title = document.getElementsByTagName("h1")[0];
+title.innerHTML = "DOM <br> IS <br> AWESOME";
+
+let tButton = document.getElementsByTagName("button")[0];
+tButton.textContent = siteContent["cta"]["button"];
+
+let secTitle = document.querySelectorAll(".main-content h4");
+let secBody = document.querySelectorAll(".main-content p");
+let arrTitle = ["features", "about", "services", "product", "vision"];
+for(i=0;i<secTitle.length;i++){
+  let secTitleWords = arrTitle[i] + "-h4";
+  secTitle[i].textContent = siteContent["main-content"][secTitleWords];
+  let secBodyWords = arrTitle[i]+ "-content";
+  secBody[i].textContent = siteContent["main-content"][secBodyWords];
+}
+
+let contH4 = document.querySelector(".contact h4");
+contH4.textContent = siteContent["contact"]["contact-h4"];
+
+let contAddr = document.querySelectorAll(".contact p")[0];
+contAddr.textContent = siteContent["contact"]["address"];
+
+let contPhone = document.querySelectorAll(".contact p")[1];
+contPhone.textContent = siteContent["contact"]["phone"];
+
+let contEmail = document.querySelectorAll(".contact p")[2];
+contEmail.textContent = siteContent["contact"]["email"];
+
+let foot = document.querySelector("footer p");
+foot.textContent = siteContent["footer"]["copyright"];
+
+let apChild = document.createElement("a");
+apChild.textContent = "Backstory";
+let append = document.querySelector("nav");
+append.appendChild(apChild);
+
+let preChild = document.createElement("a");
+preChild.textContent = "Highlights";
+let pre = document.querySelector("nav");
+pre.prepend(preChild);
+
+let navBar = document.querySelectorAll(".container nav a");
+navBar.forEach(item => item.style.color = "green");
